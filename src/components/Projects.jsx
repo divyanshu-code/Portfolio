dedb8b3c8e0a5f77366e79e7066f02228aa1a6d8
Divyanshu bisht <divyanshubisht5734@gmail.com>
2025-05-10 18:30:07 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,80 +8,80 @@ import { motion } from 'framer-motion'
 
 const projects = [
    {
-      
-      image : image2,
+
+      image: image2,
       title: 'Real Restaurant Website',
       description: 'A website for a restaurant that displays the menu and contact information also developed a fully functional with an interactive UI for customers.',
-      technologies: ['Html', 'Css', 'Tailwind Css' , 'React', 'Node.js' , 'Express.js' , 'MongoDB'],
+      technologies: ['Html', 'Css', 'Tailwind Css', 'React', 'Node.js', 'Express.js', 'MongoDB'],
       github: "https://github.com/divyanshu-code/FoodParadise"
-      
+
    },
    {
-        image : image1,
-        title: 'Employee Task Management App',
-        description: 'A web application that allows employees to create, update and delete tasks also developed a web-based system.',
-        technologies: ['Html', 'Css', 'React', 'Local Storage'],
-        github: "https://github.com/divyanshu-code/Employee_Task_Management"
-     
+      image: image1,
+      title: 'Employee Task Management App',
+      description: 'A web application that allows employees to create, update and delete tasks also developed a web-based system.',
+      technologies: ['Html', 'Css', 'React', 'Local Storage'],
+      github: "https://github.com/divyanshu-code/Employee_Task_Management"
+
 
    },
 
-    {
-            image : image3,
-            title: 'Voice chatbot',
-            description: 'A voice chatbot that can answer questions very effciently , polite intelligent and polite responses.',
-            technologies: ['Html', 'Css', 'JavaScript'],
-      github: "https://github.com/divyanshu-code/Voice-Bot-"
-    }
-    ,
-    {
-         image : image4,
-         title: ' Portfolio',
-         description: 'A portfolio website that displays about me , my projects and contact information.',
-         technologies: ['Html', 'Tailwind Css', 'React', 'Framer Motion'],
+   {
+      image: image3,
+      title: 'EcoTrack',
+      description: 'EcoTrack is react app that helps us to track our carbon emission on real time.',
+      technologies: ['Html', 'Css','Tailwind Css' ,'React', 'Node.js', 'Express.js', 'MongoDB' ,'Framer Motion' ],
+      github: "https://github.com/divyanshu-code/ecotrack"
+   }
+   ,
+   {
+      image: image4,
+      title: ' Portfolio',
+      description: 'A portfolio website that displays about me , my projects and contact information.',
+      technologies: ['Html', 'Tailwind Css', 'React', 'Framer Motion'],
       github: "https://github.com/divyanshu-code/Portfolio"
-    }
+   }
 
 ]
 
 
 
-const ProjectCard = ({project}) => {
+const ProjectCard = ({ project }) => {
    return (
 
-       
+
       <motion.div
-      initial={{opacity:0 , y:100}}
-      whileInView={{opacity:1 , y:0}}
-      viewport={{once:true}}
-      transition={{duration:0.8}}
-      
-      className='flex flex-col gap-15 items-center md:flex-row md:gap-24 '> 
-        <img src={project.image} alt="error"  className='w-full cursor-pointer rounded-2xl transition-all duration-300
+         initial={{ opacity: 0, y: 100 }}
+         whileInView={{ opacity: 1, y: 0 }}
+         viewport={{ once: true }}
+         transition={{ duration: 0.8 }}
+
+         className='flex flex-col gap-15 items-center md:flex-row md:gap-24 '>
+         <img src={project.image} alt="error" className='w-full cursor-pointer rounded-2xl transition-all duration-300
         hover:scale-105 md:w-[300px]'/>
 
-        <div className='flex flex-col gap-5'>
-             
-             <div className='flex flex-col gap-3'>
-                <div className='text-xl font-semibold'>{project.title}</div>
-                <p className='text-gray-400'>{project.description}</p>
-             </div>
+         <div className='flex flex-col gap-5'>
 
-             <div className='flex  flex-wrap gap-5'>
-                {
-                    project.technologies.map((tech , index)=>(
-                        <span key={index} className='border rounded-lg bg-black p-3'>{tech}
-                          </span>
-                          
-                    ))
-                }
+            <div className='flex flex-col gap-3'>
+               <div className='text-xl font-semibold'>{project.title}</div>
+               <p className='text-gray-400'>{project.description}</p>
+            </div>
 
-         <a href={project.github} className='border rounded-lg bg-black p-3'> Github Link </a>
-                
-             </div>
-        </div>
+            <div className='flex  flex-wrap gap-5'>
+               {
+                  project.technologies.map((tech, index) => (
+                     <span key={index} className='border rounded-lg bg-black p-3'>{tech}
+                     </span>
+
+                  ))
+               }
+
+               <a href={project.github} className='border rounded-lg bg-black p-3'> Github Link </a>
+
+            </div>
+         </div>
       </motion.div>
-      
+
    )
 
 
@@ -89,32 +89,32 @@ const ProjectCard = ({project}) => {
 
 const Projects = () => {
 
-  return (
-      
-     <div id='pro' className=' flex min-h-screen flex-col items-center justify-center gap-16 p-4 w-full md:px-14 md:py-24'>
+   return (
 
-    
-        <motion.h1
-         initial={{opacity:0 , y:100}}
-         whileInView={{opacity:1 , y:0}}
-         viewport={{once:true}}
-         transition={{duration:0.8}}
+      <div id='pro' className=' flex min-h-screen flex-col items-center justify-center gap-16 p-4 w-full md:px-14 md:py-24'>
 
-        className='text-4xl font-light text-white md:text-6xl'>My Projects</motion.h1>
-       
 
-        <div className='flex w-full max-w-[1000px] flex-col gap-16 text-white'>
+         <motion.h1
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.8 }}
 
-         {
-                projects.map((project , index)=>(
-                  <ProjectCard key={index} project={project}/>
-                ))
-                
-         }
-        </div>
-     </div>
+            className='text-4xl font-light text-white md:text-6xl'>My Projects</motion.h1>
 
-  )
+
+         <div className='flex w-full max-w-[1000px] flex-col gap-16 text-white'>
+
+            {
+               projects.map((project, index) => (
+                  <ProjectCard key={index} project={project} />
+               ))
+
+            }
+         </div>
+      </div>
+
+   )
 }
 
 export default Projects
