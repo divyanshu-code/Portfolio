9d8895891bdd76dd698494240e1bc2639d48daba
Divyanshu Bisht <divyanshubisht5734@gmail.com>
2025-07-25 12:55:17 +0530

    update

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Middle from './components/Middle'
+import Tech from './components/Tech'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+
+const App = () => {
+  return (
+
+    <>
+    
+    <main className='flex flex-col items-center px-4 md:px-8 lg:px-16'>
+      
+      <Navbar/>
+      <Middle/>
+      <Tech/>
+      <Projects/>
+      <Contact/>
+
+    </main>
+    </>
+  )
+}
+
+export default App
---
+++ components/Middle.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import image from '/public/yes.jpg'
+import {motion} from 'framer-motion'
+
+const Middle = () => {
+  return (
+    <div id='home' className='px-16 min-h-screen w-full flex  items-center justify-center py-28 md:px-32 '>
+
+      <div className='flex flex-col gap-10 items-center justify-center text-white'>
+
+        <motion.div 
+            initial={{y:-50, opacity:0}}
+            animate={ {y:0, opacity:1}}
+            transition={{ duration: 0.7 , delay:0.2}}
+        >
+          
+          <img src={image} alt="error" id='yes' className='width-[300px] cursor-pointer  shadow-xl shadow-indigo-900 transition-all duration-300  hover:-translate-y-5 hover:shadow-2xl hover:shadow-blue-600 md:w-[350px]' />
+
+        </motion.div>
+           
+        <motion.div
+        
+        initial={{y:50, opacity:0}}
+        animate={ {y:0, opacity:1}}
+        transition={{ duration: 0.7 , delay:0.2}}
+        className='flex  max-w-[600px] flex-col gap-5 items-center text-center justify-center'>
+          <h1 className='bg-gradient-to-r from-blue-600 to-red-600  bg-clip-text text-transparent  text-5xl font-light md:text-7xl'>Divyanshu</h1>
+          <h3 className='bg-gradient-to-r from-red-600 to-blue-600  bg-clip-text text-transparent  text-xl font-light md:text-3xl'> Software Engineer</h3>
+          <p className='md:text-base text-pretty text-sm text-gray-400'>
+          I'm a passionate about software engineer with a strong foundation in designing, developing. I love turning complex problems into efficient, user-friendly solutions. With expertise in javascript , React , Node.js etc.
+          I enjoy collaborating with teams, learning new technologies, and continuously improving my skills to stay ahead in the ever-evolving tech landscape.
+          Let's build something amazing together!
+          </p>
+
+           <a href=" https://drive.google.com/file/d/100asl1APm4_lNUkBlMBazJkbVX4st_ZC/view?usp=drivesdk " alt="error" className='bg-blue-500 rounded-lg  text-sm px-10 py-3 p-5 m-5 top-80 opacity-60 hover:opacity-100 '> Check My Resume </a>
+        </motion.div>
+      </div>
+    </div>
+  )
+}
+
+export default Middle
