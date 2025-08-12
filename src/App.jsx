a94384b44beac5baf9dce0fc6e75ba895b6b6627
Divyanshu bisht <divyanshubisht5734@gmail.com>
2025-08-12 23:33:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ const App = () => {
       
       <Navbar/>
       <Middle/>
-      <Tech/>
+       <div className='md:mt-24'>
+          <Tech/>
+       </div>
       <div className='mt-32 md:mt-10'>
           <Projects/>
       </div>
